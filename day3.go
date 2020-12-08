777e8523f709b46f8325e976d7f561552ff6feb0
vtsao <vtsao>
2020-12-08 12:45:35 -0500

    .

---
+++ day3.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("day3_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		entry := scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
