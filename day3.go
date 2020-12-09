1db78090a10939d41fb9dee1da7f548dd5117f35
vtsao <vtsao>
2020-12-08 20:18:11 -0500

    .

--- day3.go
+++ day3.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
+
+func trees(grid []string, right, down int) int {
+	trees := 0
+	x, y := right, down
+	for y < len(grid) {
+		if string(grid[y][x]) == "#" {
+			trees++
+		}
+		x += right
+		x %= len(grid[0])
+		y += down
+	}
+
+	return trees
+}
 
 func main() {
 	file, err := os.Open("day3_input.txt")
@@ -13,11 +29,21 @@ func main() {
 	}
 	defer file.Close()
 
+	var grid []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		entry := scanner.Text()
+		grid = append(grid, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Part 1: %d trees\n", trees(grid, 3, 1))
+
+	trees2 := trees(grid, 1, 1)
+	trees2 *= trees(grid, 3, 1)
+	trees2 *= trees(grid, 5, 1)
+	trees2 *= trees(grid, 7, 1)
+	trees2 *= trees(grid, 1, 2)
+	fmt.Printf("Part 2: %d trees\n", trees2)
 }
