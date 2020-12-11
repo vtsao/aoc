80109062164ffb93d78e28525e9ce5e141318e9f
vtsao <vtsao>
2020-12-10 22:16:27 -0500

    .

--- day6.go
+++ day6.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -13,11 +14,27 @@ func main() {
 	}
 	defer file.Close()
 
+	// For this to work we need to cheat a bit by adding an extra newline at the
+	// end of the input so we process the last group.
+	yesCount := 0
+	group := map[string]interface{}{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		scanner.Text()
+		yesses := scanner.Text()
+
+		if len(yesses) == 0 {
+			yesCount += len(group)
+			group = map[string]interface{}{}
+			continue
+		}
+
+		for _, q := range yesses {
+			group[string(q)] = nil
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Part 1: yes count: %d\n", yesCount)
 }
