be3942ad2837a9960f63e2b8d4682e3aa2f54611
vtsao <vtsao>
2020-12-10 22:23:30 -0500

    .

--- day6.go
+++ day6.go
@@ -16,25 +16,37 @@ func main() {
 
 	// For this to work we need to cheat a bit by adding an extra newline at the
 	// end of the input so we process the last group.
-	yesCount := 0
-	group := map[string]interface{}{}
+	anyYesCount := 0
+	allYesCount := 0
+	curGroup := map[string]int{}
+	curGroupLen := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		yesses := scanner.Text()
 
 		if len(yesses) == 0 {
-			yesCount += len(group)
-			group = map[string]interface{}{}
+			anyYesCount += len(curGroup)
+
+			for _, count := range curGroup {
+				if count == curGroupLen {
+					allYesCount++
+				}
+			}
+
+			curGroup = map[string]int{}
+			curGroupLen = 0
 			continue
 		}
 
+		curGroupLen++
 		for _, q := range yesses {
-			group[string(q)] = nil
+			curGroup[string(q)]++
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Part 1: yes count: %d\n", yesCount)
+	fmt.Printf("Part 1: any yes count: %d\n", anyYesCount)
+	fmt.Printf("Part 2: all yes count: %d\n", allYesCount)
 }
