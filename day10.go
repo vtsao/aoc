84510ac989b296106a4290cc4f6efd20ed841290
vtsao <vtsao>
2020-12-15 21:01:38 -0500

    .

--- day10.go
+++ day10.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
@@ -13,11 +16,30 @@ func main() {
 	}
 	defer file.Close()
 
+	var adapters []int
 	scanner := bufio.NewScanner(file)
 	for i := 0; scanner.Scan(); i++ {
-		scanner.Text()
+		adapter, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		adapters = append(adapters, adapter)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	sort.Ints(adapters)
+
+	// We start off with a diff of 3 having a count of 1, which represents our
+	// device's built-in adapter, which always has a rating of 3 higher than the
+	// highest adapter.
+	diffs := map[int]int{3: 1}
+	curJolt := 0
+	for _, adapter := range adapters {
+		diff := adapter - curJolt
+		diffs[diff]++
+		curJolt = adapter
+	}
+
+	fmt.Printf("Part 1: 1-jolt differences * 3-jolt differences: %d\n", diffs[1]*diffs[3])
 }
