12793ade16c7f267da701fc1d9e3ac0da4c7e076
Vincent <vtsao@users.noreply.github.com>
2021-12-21 01:42:58 -0500

    AoC 2021 day 21 solution. (#25)

--- 2021/day21.go
+++ 2021/day21.go
@@ -6,31 +6,129 @@ package main
 
 import (
 	"bufio"
-	"log"
+	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func parseInput() []string {
+type dice struct {
+	next int
+}
+
+func (d *dice) roll() int {
+	d.next++
+	if d.next == 101 {
+		d.next = 1
+	}
+	return d.next
+}
+
+func move(p int, d *dice) int {
+	for i := 0; i < 3; i++ {
+		p += d.roll()
+	}
+	return p % 10
+}
+
+func game1(p1, p2 int, d *dice, endGame int) (int, int, int) {
+	p1Turn := true
+	p1Score, p2Score := 0, 0
+	diceRolls := 0
+	for {
+		if p1Turn {
+			p1 = move(p1, d)
+			p1Score += p1 + 1
+			diceRolls += 3
+			if p1Score >= endGame {
+				break
+			}
+			p1Turn = false
+			continue
+		}
+		p2 = move(p2, d)
+		p2Score += p2 + 1
+		diceRolls += 3
+		if p2Score >= endGame {
+			break
+		}
+		p1Turn = true
+	}
+
+	return p1Score, p2Score, diceRolls
+}
+
+type key struct {
+	p1, p2, p1Score, p2Score int
+	p1Turn                   bool
+}
+
+type wins struct {
+	p1, p2 int
+}
+
+func game2(p1, p2, p1Score, p2Score, endGame int, p1Turn bool, cache map[key]wins) (int, int) {
+	if p1Score >= endGame {
+		return 1, 0
+	}
+	if p2Score >= endGame {
+		return 0, 1
+	}
+	k := key{p1: p1, p2: p2, p1Score: p1Score, p2Score: p2Score, p1Turn: p1Turn}
+	if wins, ok := cache[k]; ok {
+		return wins.p1, wins.p2
+	}
+
+	p1Wins, p2Wins := 0, 0
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			for k := 1; k <= 3; k++ {
+				roll := i + j + k
+				if p1Turn {
+					newP1 := p1 + roll
+					newP1 %= 10
+					newP1Score := p1Score + newP1 + 1
+					p1WinsR, p2WinsR := game2(newP1, p2, newP1Score, p2Score, endGame, false, cache)
+					p1Wins += p1WinsR
+					p2Wins += p2WinsR
+					continue
+				}
+				newP2 := p2 + roll
+				newP2 %= 10
+				newP2Score := p2Score + newP2 + 1
+				p1WinsR, p2WinsR := game2(p1, newP2, p1Score, newP2Score, endGame, true, cache)
+				p1Wins += p1WinsR
+				p2Wins += p2WinsR
+			}
+		}
+	}
+
+	cache[k] = wins{p1: p1Wins, p2: p2Wins}
+	return p1Wins, p2Wins
+}
+
+func parseInput() (int, int) {
 	file, _ := os.Open("day21_input.txt")
 	defer file.Close()
 
-	var lines []string
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
+	scanner.Scan()
+	p1, _ := strconv.Atoi(strings.TrimPrefix(scanner.Text(), "Player 1 starting position: "))
+	scanner.Scan()
+	p2, _ := strconv.Atoi(strings.TrimPrefix(scanner.Text(), "Player 2 starting position: "))
 
-	return lines
+	return p1, p2
 }
 
 func main() {
-	lines := parseInput()
+	p1, p2 := parseInput()
+	p1--
+	p2--
 
-	for _, line := range lines {
-		log.Println(line)
-	}
+	_, p2Score, diceRolls := game1(p1, p2, &dice{}, 1000)
+	fmt.Printf("Part 1: %d\n", p2Score*diceRolls)
 
-	// fmt.Printf("Part 1: %d\n", )
-	// fmt.Printf("Part 2: %d\n", )
+	p1Wins, p2Wins := game2(p1, p2, 0, 0, 21, true, map[key]wins{})
+	fmt.Printf("Part 2: %d\n", int(math.Max(float64(p1Wins), float64(p2Wins))))
 }
