18b19ab3d116466df2e8776b4e7fdb1cd16bcb05
Vincent <vtsao@users.noreply.github.com>
2022-12-24 23:53:50 +0000

    .

---
+++ 2022/day18.go
@@ -0,0 +1,15 @@
+// This program implements the solution for
+// https://adventofcode.com/2022/day/18.
+package main
+
+import (
+	_ "embed"
+)
+
+//go:embed day18_input.txt
+var jetPattern string
+
+func main() {
+	// fmt.Printf("Part 1: %d\n", )
+	// fmt.Printf("Part 2: %d\n", )
+}
